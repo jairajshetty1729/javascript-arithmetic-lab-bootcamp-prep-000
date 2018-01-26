aae89e6862854ad87c2df7bd98d3ea88b9e3404e
jairaj shetty <jayuchintu@gmail.com>
2018-01-26 06:16:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function add(a,b){
   var sum=a+b;
   return sum;
 }
+function subtract(a,b){
+  var diffference=a-b;
+  return difference;
+}
+function multiply(a,b){
+  var mul=a*b;
+  return mul;
+}
